5695014ceaa9f0235a9563d3416503322f389716
AlyssaWang <alyssa.c.wang@gmail.com>
2018-10-17 23:53:16 +0100

    doubleside broken

--- planes.js
+++ planes.js
@@ -1,6 +1,6 @@
-var side = 20;
+var length = 20;
 
-var plane = new THREE.PlaneGeometry(side, side, side);
+var plane = new THREE.PlaneGeometry(length, length, length);
 
 var materialRed = new THREE.MeshPhongMaterial({color: 0xB22222, side: THREE.DoubleSide});
 var materialGreen = new THREE.MeshPhongMaterial({color: 0x006400, side: THREE.DoubleSide});
@@ -8,20 +8,20 @@ var materialGrey = new THREE.MeshPhongMaterial({color: 0xA9A9A9, side: THREE.Dou
 
 // Drawing planes for the box
 var planeLeft = new THREE.Mesh(plane, materialRed);
-planeLeft.position.set(-side/2, 0, 0);
+planeLeft.position.set(-length/2, 0, 0);
 planeLeft.rotation.y = Math.PI/2; // 90 degree rotation
 
 var planeRight = new THREE.Mesh(plane, materialGreen);
-planeRight.position.set(side/2, 0, 0);
+planeRight.position.set(length/2, 0, 0);
 planeRight.rotation.y = Math.PI/2; // 90 degree rotation
 
 var planeTop = new THREE.Mesh(plane, materialGrey);
-planeTop.position.set(0, side/2, 0);
+planeTop.position.set(0, length/2, 0);
 planeTop.rotation.x = Math.PI/2; // 90 degree rotation
 
 var planeBottom = new THREE.Mesh(plane, materialGrey);
-planeBottom.position.set(0, -side/2, 0);
+planeBottom.position.set(0, -length/2, 0);
 planeBottom.rotation.x = Math.PI/2; // 90 degree rotation
 
 var planeBack = new THREE.Mesh(plane, materialGrey);
-planeBack.position.set(0, 0, -side/2);
+planeBack.position.set(0, 0, -length/2);
