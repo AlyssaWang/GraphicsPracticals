99b6784abb1adf4f921e068aef82a9ba3219dfda
AlyssaWang <alyssa.c.wang@gmail.com>
2018-10-27 20:33:08 +0100

    sphere values

--- sphere.js
+++ sphere.js
@@ -1,4 +1,6 @@
-var sphere = new THREE.SphereGeometry(2);
+// var sphere = new THREE.SphereGeometry(2);
+var sphere = new THREE.SphereGeometry(0, 30, 30, 20, Math.PI * 2, 0 Math.PI * 2);
+
 var material = new THREE.MeshPhongMaterial({color: 0x4682B4, wireframe: false});
 var sphereMesh = new THREE.Mesh(sphere, material);
 sphereMesh.position.set(5, 0, 0);
